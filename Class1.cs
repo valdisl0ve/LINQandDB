5fad3bdb51c9803b101c9b7f02e72d4ed7ab8baa
valdisl0ve <75386722+valdisl0ve@users.noreply.github.com>
2021-07-01 21:30:00 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace dll
+{
+    public class Class1
+    {
+        const string connection = "";
+        MySqlConnection db = new MySqlConnection(connection);
+        
+        List<User> usersList = new List<User>();
+
+        public void ImportFromDB()
+        {
+            
+            db = new MySqlConnection(connection);
+            db.Open();
+            MySqlCommand command = new MySqlCommand($"SELECT * FROM table_USER", db);
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string temp1 = reader["NAME"].ToString();
+                string temp2 = reader["SURNAME"].ToString();
+                string temp3 = reader["CONTACT"].ToString();
+                usersList.Add(new User(temp1, temp2, temp3));
+            }
+            db.Close();
+        }
+
+
+
+        public void ChangeSomething(string findThis, string changeToThis)
+        {
+                var select = from user in usersList
+                    where user.name == findThis
+                    where user.surName == findThis
+                    where user.contact == findThis
+                    select (user.name, user.surName, user.contact);
+
+
+                if (select.Equals(findThis))
+                {
+                    
+                }
+                
+
+
+
+
+
+        }
+        
+        
+        
+        
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿namespace dll
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string surName { get; set; }
+        public string contact { get; set; }
+
+
+        public User()
+        {
+        }
+
+        public User(string temp1, string temp2, string temp3)
+        {
+            name = temp1;
+            surName = temp2;
+            contact = temp3;
+        }
+    }
+}
